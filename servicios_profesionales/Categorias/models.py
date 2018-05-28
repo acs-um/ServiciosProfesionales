9b41e78a7a2f9bc08d6ae226c80ee5ee289bac48
leansabio <leansabio@gmail.com>
2018-05-28 15:37:40 -0300

    Arreglado el modelo categoria

--- servicios_profesionales/Categorias/models.py
+++ servicios_profesionales/Categorias/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here
-class categoria(models.Model):
-    name=models.CharField(max_length=200, help_text="Nombre de la clase")
+class Categoria(models.Model):
+    name=models.CharField(max_length=20, help_text="Nombre de la Categoria")
+    
+
 
 
