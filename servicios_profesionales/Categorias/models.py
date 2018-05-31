3fd16ccc4998fc9e07a1f500e6705b744b3e96d2
leansabio <leansabio@gmail.com>
2018-05-31 10:29:54 -0300

    agregado str al modelo

--- servicios_profesionales/Categorias/models.py
+++ servicios_profesionales/Categorias/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here
 class Categoria(models.Model):
     name=models.CharField(max_length=20, help_text="Nombre de la Categoria")
+    description = models.TextField(max_length=300, null=True)
+
+    def __str__ (self):
+        return self.name
+
     
 
 
