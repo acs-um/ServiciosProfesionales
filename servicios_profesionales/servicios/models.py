0c9f8831794d08cf2c85066be6b802c9944f92f2
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-05-31 11:08:15 -0300

    Corregido manejo de tags y servicios

--- servicios_profesionales/servicios/models.py
+++ servicios_profesionales/servicios/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+from Categorias.models import Categoria
 from taggit.managers import TaggableManager
 
 class Service(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField
-
+    category = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     tags = TaggableManager()
 
     def __str__(self):
-        return self.name
+        return "{},{}".format(self.name, self.category)
--- servicios_profesionales/servicios_profesionales/settings.py
+++ servicios_profesionales/servicios_profesionales/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'servicios',
     'comentarios',
+    'Categorias',
     'photologue',
     'sortedm2m',
     'taggit',
