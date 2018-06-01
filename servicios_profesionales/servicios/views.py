d14fb61488c702905104ce997b3a7111d2dc70dd
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-06-01 19:03:13 -0300

    Aumentado cantidad de caracteres en nombre de Categoria Creado template servicios Añadido redireccionamiento desde urls.py a views de servicios Creadas las views de Servicios Actualizado link del NAV a servicios

--- servicios_profesionales/servicios/views.py
+++ servicios_profesionales/servicios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Service
 
-# Create your views here.
+
+def index(request):
+    service_list = Service.objects.all()
+    context = {'service_list': service_list}
+    return render(request, 'servicios/index.html', context)
