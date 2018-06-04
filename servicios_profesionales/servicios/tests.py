388dce01352af9ad41b62ba812e16abbca11a8ff
Matias Varela <matu.varela@gmail.com>
2018-06-04 18:33:19 -0300

    agrego tests al listado de servicios

--- servicios_profesionales/servicios/tests.py
+++ servicios_profesionales/servicios/tests.py
@@ -1,3 +1,37 @@
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+from servicios.models import Service
+from Categorias.models import Categoria
+
+class ServiceViewTests(TestCase):
+
+    def setUp(self):
+        self.categoria = Categoria.objects.create(
+            name='Construcción',
+            description='Trabajos de construcción')
+        self.service = Service.objects.create(
+            name='Albañil',
+            description='Servicios generales',
+            category=self.categoria
+        )
+        self.service2 = Service.objects.create(
+            name='Yesero',
+            description='Yesería en general',
+            category=self.categoria
+        )
+
+    def test_service_list(self):
+        resp = self.client.get(reverse('all-services'))
+        self.assertEqual(resp.status_code, 200)
+
+        # que los servicios estén en el contexto
+        self.assertEqual(resp.context["service_list"].count(), 2)
+        self.assertTrue(self.service in resp.context["service_list"])
+        self.assertTrue(self.service2 in resp.context["service_list"])
+
+        # que los nombres de los servicios estén en el html
+        self.assertContains(resp, self.service.name)
+        self.assertContains(resp, self.service2.name)
+
