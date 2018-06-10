ea7cfa9618bc075d7562ade8db5edd18767ed575
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-06-09 21:33:28 -0300

    Implementando sesiones

--- servicios_profesionales/usuarios/urls.py
+++ servicios_profesionales/usuarios/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.decorators import login_required
+
 from . import views
 
 urlpatterns = [
-    path('reset_password', PasswordResetConfirmView.as_view()),
-    path('ingresar', views.signup, name = "SignUp"),
+    path('reset_password', login_required(PasswordResetConfirmView.as_view())),
+    path('ingresar/', views.signup, name="SignUp"),
+    path('ingresar/login/', views.login, name="EmailLogin"),
+    path('', include('django.contrib.auth.urls')),
+    path('accounts/profile/', views.get_user_profile, name='update-person'),
+    path('cerrar-sesion', views.SignOutView.as_view(), name='sign_out')
 ]
