c69e539685b13d2dbdd0e7ee7670628e52bd527b
Bruseghini92 <bruseghini92.ab@gmail.com>
2018-06-09 23:43:33 -0300

    Añadido login_required para alta de servicios

--- servicios_profesionales/servicios/urls.py
+++ servicios_profesionales/servicios/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="all-services"),
-    #url(r'^search/$', views.search, name="search-service"),
-    url(r'^crear$', views.CreateService.as_view(), name="createService"),
+    # url(r'^search/$', views.search, name="search-service"),
+    url(r'^crear$', login_required(views.CreateService.as_view()), name="createService"),
     url(r'^modificar/(?P<pk>\d+)/$', views.UpdateService.as_view(), name="updateService"),
     url(r'^borrar/(?P<pk>\d+)/$', views.DeleteService.as_view(), name="deleteService"),
-    #path('ingresar', views.signup, name = "SignUp"),
+    # path('ingresar', views.signup, name = "SignUp"),
 ]
